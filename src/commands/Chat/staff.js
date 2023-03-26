d35a0e0e0e562fbc79d26cd27e4dbb80b0aaca63
ThatBadName <90130690+WalkerOfDarkness@users.noreply.github.com>
2023-03-26 20:38:44 +0100

    .

--- src/commands/Chat/staff.js
+++ src/commands/Chat/staff.js
@@ -11,6 +11,6 @@ module.exports = {
   async execute(message, args, client) {
     const cooldown = Handler.main.cooldowns(client, 'staff', {id: 994642021425877112, personal: 600})
     if (cooldown && cooldown.cancelCommand === true) return message.reply({content: `Staff have been pinged recently`})
-    message.channel.send({content: `<@&1051191671494291547>\nPinged by: ${message.author}\nReason: ${args[0].slice(0, 500)}`})
+    message.channel.send({content: `<@&1051191671494291547>\nPinged by: ${message.author}\nReason: ${args.length > 0 ? args[0].slice(0, 500) : 'Aww no reason'}`})
   }
 }
